4199430f7c1187cd41ac942f9643180b58f5632e
Billy Dominique <billydominique@pursuit.org>
2022-02-06 06:40:21 -0500

    removed node_modeules

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const app = require('./app');
 
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 7077;
 
 app.listen(PORT, () => {
     console.log(`🧾 Listening on port ${PORT} 🧾`);
